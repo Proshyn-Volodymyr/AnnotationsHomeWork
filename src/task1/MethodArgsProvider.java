986ec978422ac40a7130f633f26fcc24d51ba695
Vladimir Proshin <VlaShin1986@gmail.com>
2023-02-21 21:56:00 +0200

    Added custom annotation

---
+++ src/task1/MethodArgsProvider.java
@@ -0,0 +1,13 @@
+package task1;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodArgsProvider {
+    int param1 = 2;
+    int param2 = 5;
+}
