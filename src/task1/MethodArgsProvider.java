79ef00b621cd0f94dd3d8a97c18c9dbb4790fc04
Vladimir Proshin <VlaShin1986@gmail.com>
2023-02-22 22:08:12 +0200

    Changed annotation parameter

--- src/task1/MethodArgsProvider.java
+++ src/task1/MethodArgsProvider.java
@@ -8,6 +8,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MethodArgsProvider {
-    int param1 = 2;
-    int param2 = 5;
+    int param1() default 10;
+    int param2() default 25;
 }
