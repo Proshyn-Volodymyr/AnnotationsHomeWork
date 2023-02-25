8c24d32c37dcd9939693340b60063d186c4d7e06
Vladimir Proshin <VlaShin1986@gmail.com>
2023-02-25 18:20:47 +0200

    run the application with test parameters

---
+++ src/task3/Main.java
@@ -0,0 +1,34 @@
+package task3;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Class carClass = car.getClass();
+        String path = "task3.ser";
+        File file = new File(path);
+
+        car.setBrand("BMW");
+        car.setEngineType("V8");
+        car.setPrice(50000);
+        car.setAvailable(true);
+        car.setModel("X5");
+        try {
+            ClassWriter.saveToJson(car, path);
+        } catch (IOException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Object carObject = ClassReader.readFromJson(file, carClass);
+            System.out.println(carObject);
+        } catch (NoSuchFieldException | InvocationTargetException | InstantiationException | NoSuchMethodException |
+                 IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
