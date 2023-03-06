b75e1ebb036c393de02f1b7e5658452432b9e34e
Vladimir Proshin <VlaShin1986@gmail.com>
2023-03-06 21:32:28 +0200

    added empty constructor to Transmission class

---
+++ src/task3/Transmission.java
@@ -0,0 +1,31 @@
+package task3;
+
+public class Transmission {
+
+    private String type;
+
+    private int amountOfSpeed;
+
+    public Transmission(String type, int amountOfSpeed) {
+        this.type = type;
+        this.amountOfSpeed = amountOfSpeed;
+    }
+    public Transmission() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAmountOfSpeed() {
+        return amountOfSpeed;
+    }
+
+    public void setAmountOfSpeed(int amountOfSpeed) {
+        this.amountOfSpeed = amountOfSpeed;
+    }
+}
