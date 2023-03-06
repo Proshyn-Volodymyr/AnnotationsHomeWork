2ba09a90be5405048d7ee7711e14e5b4805838ac
Vladimir Proshin <VlaShin1986@gmail.com>
2023-03-06 22:34:29 +0200

    created instances of classes for running program

--- src/task3/Main.java
+++ src/task3/Main.java
@@ -1,32 +1,43 @@
 package task3;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        Class carClass = car.getClass();
-        String path = "task3.ser";
-        File file = new File(path);
-
+        Transmission transmission = new Transmission("manual", 5);
+        Car car = new Car(transmission);
         car.setBrand("BMW");
         car.setEngineType("V8");
-        car.setPrice(50000);
+        car.setPrice(60000);
         car.setAvailable(true);
-        car.setModel("X5");
+        car.setModel("X6");
+
+
+        Class carClass = car.getClass();
+        String path = "task3.txt";
+        File file = new File(path);
+        ClassReader classReader = new ClassReader();
+        String jsonString = "";
+
+
         try {
-            ClassWriter.saveToJson(car, path);
+            jsonString = ClassWriter.saveToJson(car, path);
         } catch (IOException | IllegalAccessException e) {
             e.printStackTrace();
         }
 
+        try(FileWriter fileWriter = new FileWriter(path)) {
+            fileWriter.write(jsonString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         try {
-            Object carObject = ClassReader.readFromJson(file, carClass);
+            Object carObject = classReader.readFromJsonFile(file, car);
             System.out.println(carObject);
-        } catch (NoSuchFieldException | InvocationTargetException | InstantiationException | NoSuchMethodException |
-                 IllegalAccessException e) {
+        } catch (InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
         }
 
